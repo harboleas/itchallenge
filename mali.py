e2220535b9034d32bb2563de9f08fc86648bfee4
Hugo Arboleas <harboleas@citedef.gob.ar>
2018-01-04 14:17:15 -0300

    modif. calcula_saltos de mali.py

--- mali.py
+++ mali.py
@@ -2,7 +2,7 @@
 def contar_coreos(n, k=0, tablero=None):
 
     if k == 0:
-        tablero = [[0 for j in xrange(n)] for i in xrange(n)]
+        tablero = [[None for j in xrange(n)] for i in xrange(n)]
 
     coreos = 0
     i = k / n
@@ -15,6 +15,8 @@ def contar_coreos(n, k=0, tablero=None):
 
     else:
         if k == n**2 - 1:
+            tablero[i][j] = saltos_posibles[0]
+            print tablero
             return len(saltos_posibles)
         else:
             for salto in saltos_posibles:
@@ -31,7 +33,6 @@ def calcular_saltos(mat, i, j):
         return [">", "v"]
 
     else:
-        posibles = [">", "^", "<", "v"]
 
         #     | j-2  j-1   j   j+1 
         # ------------------------
@@ -39,42 +40,54 @@ def calcular_saltos(mat, i, j):
         # i-1 |       B    C    D
         #  i  |  E    F  (i,j)
 
-        entran_C = 0
-        entran_F = 0
-        c = None
-        f = None
-        if i-2 >= 0 and mat[i-2][j] == "v":
-            entran_C += 1
+        a = b = c = d = e = f = None
+
         if i-1 >= 0:
             c = mat[i-1][j]
+            if i-2 >= 0:
+                a = mat[i-2][j]
             if j-1 >= 0:
-                if mat[i-1][j-1] == ">":
-                    entran_C += 1
-                elif mat[i-1][j-1] == "v":
-                    entran_F += 1
-            if j+1 <= n-1 and mat[i-1][j+1] == "<":
-                entran_C += 1
-        if j-2 >= 0 and mat[i][j-2] == ">":
-            entran_F += 1
+                b = mat[i-1][j-1]
+            if j+1 < n:
+                d = mat[i-1][j+1]
 
         if j-1 >= 0:
             f = mat[i][j-1]
+            if j-2 >= 0:
+                e = mat[i][j-2]
 
-        if entran_C > 0 or c == "v" or i == 0:
+        entran_C = 0
+        if a == "v" or b == ">" or d == "<":
+            entran_C = 1
+
+        entran_F = 0
+        if b == "v" or e == ">":
+            entran_F = 1
+
+#        if c == "v" and entran_C == 0:
+#            return []
+#        if f == ">" and i == n-1 and entran_F == 0:
+#            return []
+#
+#        if c and f and entran_C == 0 and i == n-1 and entran_F == 0:
+#            return []
+#
+#        if c and entran_C == 0:
+#            return ["^"]
+#
+#        if f and i == n-1 and entran_F == 0:
+#            return ["<"]
+
+        posibles = [">", "^", "<", "v"]
+        if entran_C == 1 or c == "v" or i == 0:
             posibles.remove("^")
-        if entran_F > 0 or f == ">" or j == 0:
+        if entran_F == 1 or f == ">" or j == 0:
             posibles.remove("<")
         if i == n-1:
             posibles.remove("v")
-        if j == n-1:
+        if j == n-1 or d == "v":
             posibles.remove(">")
 
-        if entran_C == 0:
-            if i == n-1 and entran_F == 0:
-                posibles = []
-            else:
-                posibles = ["^"]
-        elif 
         return posibles
 
 #  vim: set ts=4 sw=4 tw=79 et :
