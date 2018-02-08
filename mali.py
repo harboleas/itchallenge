fd5cae00e3cc1f5a673802c0a7a77dfb30433d3b
Hugo Arboleas <harboleas@citedef.gob.ar>
2018-02-07 22:13:20 -0300

    modif. mali.py

--- mali.py
+++ mali.py
@@ -1,9 +1,10 @@
-
-pepe = [0]
+llamadas = [0]
 
 def contar_coreos(n, k=0, tablero=None):
+    """Cuenta las coreografias de las pulgas
+       generandolas de forma recursiva"""
 
-    pepe[0] += 1
+    llamadas[0] += 1
 
     if k == 0:
         tablero = [[None for j in xrange(n)] for i in xrange(n)]
@@ -12,108 +13,116 @@ def contar_coreos(n, k=0, tablero=None):
     i = k / n
     j = k % n
 
-    saltos_posibles = calcular_saltos(tablero, i, j)
+    saltos_posibles = determinar_saltos(tablero, i, j)
 
     if not saltos_posibles:
-#        print i+1,j+1
-#        for f in tablero:
-#            print f
-        return 0
+       return 0
 
     else:
         if k == n**2 - 1:
             tablero[i][j] = saltos_posibles[0]
-#            print i+1,j+1
-#            for f in tablero:
-#                print f
+#            print "\ntablero"
+#            for fila in tablero:
+#                for col in fila:
+#                    print col,
+#                print "\n"
             return 1
         else:
-#            print i+1,j+1
-#            print "posibles ", saltos_posibles
             for salto in saltos_posibles:
-#                raw_input()
-#                print i+1,j+1
                 tablero[i][j] = salto
-#                for f in tablero:
-#                    print f
                 coreos += contar_coreos(n, k+1, tablero)
             return coreos
 
 
-def calcular_saltos(mat, i, j):
+class list_(list):
+    """Solo modifico el comportamiento del remove"""
+
+    def remove(self, elem):
+
+        try:
+            list.remove(self, elem)
+        except:
+            pass
+
+
+def determinar_saltos(mat, i, j):
+    """Determina los posibles saltos entrantes y salientes a la casilla"""
 
     n = len(mat)
 
-    if i == j == 0:
-        return ["v", ">"]
+    # posibles = ["-: ", "_: ", " :_", " :-", "---", " | "]
 
-    else:
+    #     |  j-1   j   j+1 
+    # --------------------
+    # i-1 |        A    C 
+    #  i  |   B  (i,j) 
 
-        #     | j-2  j-1   j   j+1 
-        # ------------------------
-        # i-2 |            A
-        # i-1 |       B    C    D
-        #  i  |  E    F  (i,j)
+    a = b = c = d = None
 
-        a = b = c = d = e = f = None
+    if i-1 >= 0:
+        a = mat[i-1][j]
 
-        if i-1 >= 0:
-            c = mat[i-1][j]
-            if i-2 >= 0:
-                a = mat[i-2][j]
-            if j-1 >= 0:
-                b = mat[i-1][j-1]
-            if j+1 < n:
-                d = mat[i-1][j+1]
+    if j-1 >= 0:
+        b = mat[i][j-1]
 
-        if j-1 >= 0:
-            f = mat[i][j-1]
-            if j-2 >= 0:
-                e = mat[i][j-2]
+    if i-1 >= 0 and j+1 <= n-1:
+        c = mat[i-1][j+1]
 
-        entran_C = 0
-        if a == "v" or b == ">" or d == "<":
-            entran_C = 1
+    ###########################################
 
-        entran_F = 0
-        if b == "v" or e == ">":
-            entran_F = 1
+    posibles = list_(["-: ", "_: ", " :_", " :-", "---", " | "])
 
-        entran = 0
-        if f == ">" or c == "v":
-            entran = 1
+    if i == 0:
+        posibles.remove(" | ")
+        posibles.remove("_: ")
+        posibles.remove(" :_")
 
-        ###########################################
+    if j == 0:
+        posibles.remove("---")
+        posibles.remove("_: ")
+        posibles.remove("-: ")
 
-        if c and f and not entran_C and not entran_F:
-            if f == "v" or c == "v":
-               return []
-            else:
-                return ["^"]
+    if i == 0 and j == n-2:
+        posibles.remove("-: ")
 
-        elif c and not entran_C:
-            if c == "v":
-               return []
-            else:
-                return ["^"]
+    if j == 0 and i == n-2:
+        posibles.remove(" :_")
 
-        elif f and not entran_F:
-            if f == "v":
-                return ["<"]
+    if i == n-1:
+        posibles.remove(" | ")
+        posibles.remove("-: ")
+        posibles.remove(" :-")
 
-        posibles = [">", "^", "<", "v"]
+    if j == n-1:
+        posibles.remove("---")
+        posibles.remove(" :_")
+        posibles.remove(" :-")
 
-        if entran_C or c == "v" or i == 0:
-            posibles.remove("^")
-        if entran_F or f == ">" or j == 0 or (i == n-2 and j == 1 and f ==
-                "^"):
-            posibles.remove("<")
-        if i == n-1 or (j == n-1 and not entran) or (i == 0 and j == n-2 and f
-                == ">") :
-            posibles.remove("v")
-        if j == n-1 or d == "v" or (j == 0 and i == n-2 and c == "v"):
-            posibles.remove(">")
+    if c == "_: " and j == n-2:
+        posibles.remove(" | ")
+        posibles.remove("_: ")
+        posibles.remove("-: ")
 
-        return posibles
+    if a :
+        if a == "---" or a == "_: " or a == " :_":
+            posibles.remove("_: ")
+            posibles.remove(" :_")
+            posibles.remove(" | ")
+        else:
+            posibles.remove("-: ")
+            posibles.remove(" :-")
+            posibles.remove("---")
+
+    if b:
+        if b == " | " or b == "_: " or b == "-: ":
+            posibles.remove("---")
+            posibles.remove("_: ")
+            posibles.remove("-: ")
+        else:
+            posibles.remove(" | ")
+            posibles.remove(" :-")
+            posibles.remove(" :_")
+
+    return posibles
 
 #  vim: set ts=4 sw=4 tw=79 et :
