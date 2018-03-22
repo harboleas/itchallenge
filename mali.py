492162ad0de1ffb8878b3cff493dafff8d25c5d5
Hugo Arboleas <harboleas@citedef.gob.ar>
2018-03-22 16:00:15 -0300

    mali dibuja 1era etapa ok

--- mali.py
+++ mali.py
@@ -6,13 +6,14 @@ def base2(n, size):
 
 def dibujaGrafo(i, N):
     """Dibuja un grafo que representa un conjunto de coreos"""
+
     n = N / 2
     h = n-1
     v = n
     tot_e = h*n + v*(n-1)
     G = base2(i, tot_e)
     m = 2*n-1
-    for j in range(m-1):
+    for j in range(n):
         H = G[j*m:(j+1)*m][:h]
         fila = "*"
         for a in H:
@@ -22,7 +23,7 @@ def dibujaGrafo(i, N):
                 fila += "  *"
         print fila
 
-        V = G[j*m:(j+1)*m][h:v+1]
+        V = G[j*m:(j+1)*m][h:v+h]
         fila = ""
         for a in V:
             if a == "1":
