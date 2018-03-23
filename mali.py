d6b61f2d7cf71b9e8e64503e934648fb512663c8
Hugo Arboleas <harboleas@citedef.gob.ar>
2018-03-23 11:23:10 -0300

    modif mali

--- mali.py
+++ mali.py
@@ -1,3 +1,27 @@
+# Puede pensarse que una coreografia (sin considerar los sentidos de giro) 
+# en un tablero de NxN, es un subgrafo del grafo compuesto por los 
+# vertices y aristas del tablero original sin las casillas del borde.
+
+# Esto es, existe una biyeccion entre el conjunto de coreos y
+# un subconjunto de todos los subgrafos
+
+####################################################################
+# Ejemplo de coreografia valida en un Tablero de 4x4, con su grafo:
+#
+#  X --> X     X --> X
+#  |  *  |     |  *  |
+#  X <-- X     X  :  X 
+#              |  :  |
+#  X --> X --> X  :  X
+#  |  *...........*  |
+#  X <-- X <-- X <-- X
+# 
+# Las X y las flechas representan las pulgas el sentido del salto,
+# y los asteriscos y puntos, el subgrafo 
+
+# Esto es, existe una funcion biyectiva entre el conjunto de coreos y 
+# un subconjunto de todos los subgrafos.
+
 
 def base2(n, size):
 
