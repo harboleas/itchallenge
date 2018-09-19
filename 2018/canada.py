d763f2d7461e323fa279928118e58249e39716c8
Hugo Arboleas <harboleas@citedef.gob.ar>
2018-09-19 15:35:48 -0300

    modif canada

--- 2018/canada.py
+++ 2018/canada.py
@@ -14,22 +14,17 @@ img = cv2.imread("ImagenIndescifrable.png")
 cuadros = [[img[i*50:i*50+50, j*50:j*50+50,:] for j in range(20)] for i in range(20)]
 
 ##############
-# A   B    C #
+# A        B #
 #            #
-# D        E #
 #            #
-# F   G    H #
+#            #
+# C        D #
 ##############
 
 A = (2, 2)
-B = (2, 25)
-C = (2, 47)
-D = (25, 2)
-E = (25, 47)
-F = (47, 2)
-G = (47, 25)
-H = (47, 47)
-
+B = (2, 47)
+C = (47, 2)
+D = (47, 47)
 
 
 def ordenar(cuadros, orden_mat):
@@ -42,6 +37,7 @@ def ordenar(cuadros, orden_mat):
             imag_out[i*50:i*50+50,j*50:j*50+50,:] = cuadros[ii][jj]
 
     return imag_out
+
 
 orden_mat = np.zeros((20,20,2), dtype = np.uint8)
 
@@ -58,51 +54,58 @@ def gen_posibles(n):
         k = t % 20
         aux.remove(tuple(orden_mat[h,k]))
 
+
     if i == 0 and 0 < j:
         ii, jj = orden_mat[i,j-1]
         cuadro = cuadros[ii][jj]
 
-        c1 = cuadro[2:47, 47]
+        c1 = cuadro[B]
+        c2 = cuadro[D]
 
         posibles_sig = []
 
         for p,q in aux:
             aux_cuad = cuadros[p][q]
-            if np.all(c1 == aux_cuad[2:47, 2]):
-                posibles_sig.append((p,q))
+            if np.all(c1 == aux_cuad[A]):
+                if np.all(c2 == aux_cuad[C]):
+                    posibles_sig.append((p,q))
         return posibles_sig
 
     elif i > 0 and j == 0:
         ii, jj = orden_mat[i-1,j]
         cuadro = cuadros[ii][jj]
 
-        c1 = cuadro[47, 2:47]
+        c1 = cuadro[C]
+        c2 = cuadro[D]
 
         posibles_sig = []
 
         for p,q in aux:
             aux_cuad = cuadros[p][q]
-            if np.all(c1 == aux_cuad[2, 2:47]):
-                posibles_sig.append((p,q))
+            if np.all(c1 == aux_cuad[A]):
+                if np.all(c2 == aux_cuad[B]):
+                    posibles_sig.append((p,q))
         return posibles_sig
 
     else:
-        i1, j1 = orden_mat[i, j-1]
-        i2, j2 = orden_mat[i-1, j]
+        i1, j1 = orden_mat[i,j-1]
+        i2, j2 = orden_mat[i-1,j]
 
         cuad1 = cuadros[i1][j1]
         cuad2 = cuadros[i2][j2]
 
-        c1 = cuad1[2:47, 47]
-        c2 = cuad2[47, 2:47]
+        c1 = cuad1[B]
+        c2 = cuad1[D]
+        c3 = cuad2[D]
 
         posibles_sig = []
 
         for p,q in aux:
             aux_cuad = cuadros[p][q]
-            if np.all(c1 == aux_cuad[2:47, 2]):
-                if np.all(c2 == aux_cuad[2, 2:47]):
-                    posibles_sig.append((p,q))
+            if np.all(c1 == aux_cuad[A]):
+                if np.all(c2 == aux_cuad[C]):
+                    if np.all(c3 == aux_cuad[B]):
+                        posibles_sig.append((p,q))
         return posibles_sig
 
 
@@ -132,10 +135,98 @@ def buscar_orden(n=0, posibles=None):
             if not (aux is None):
                 return aux
 
+
 def show_imag():
 
     cv2.imshow(" ",ordenar(cuadros, orden_mat))
     cv2.waitKey(1)
 
+
+def posibles_der(pos):
+
+    i,j = pos
+    cuad = cuadros[i][j]
+    c1 = cuad[B]
+    c2 = cuad[D]
+    posibles = []
+    aux = [(h,k) for h in range(20) for k in range(20)]
+    aux.remove(pos)
+    for q,p in aux:
+        cuad_aux = cuadros[q][p]
+        if np.all(c1 == cuad_aux[A]):
+            if np.all(c2 == cuad_aux[C]):
+                posibles.append((q,p))
+
+    return posibles
+
+def posibles_izq(pos):
+
+    i,j = pos
+    cuad = cuadros[i][j]
+    c1 = cuad[A]
+    c2 = cuad[C]
+    posibles = []
+    aux = [(h,k) for h in range(20) for k in range(20)]
+    aux.remove(pos)
+    for q,p in aux:
+        cuad_aux = cuadros[q][p]
+        if np.all(c1 == cuad_aux[B]):
+            if np.all(c2 == cuad_aux[D]):
+                posibles.append((q,p))
+
+    return posibles
+
+
+def posibles_arr(pos):
+
+    i,j = pos
+    cuad = cuadros[i][j]
+    c1 = cuad[A]
+    c2 = cuad[B]
+    posibles = []
+    aux = [(h,k) for h in range(20) for k in range(20)]
+    aux.remove(pos)
+    for q,p in aux:
+        cuad_aux = cuadros[q][p]
+        if np.all(c1 == cuad_aux[C]):
+            if np.all(c2 == cuad_aux[D]):
+                posibles.append((q,p))
+
+    return posibles
+
+def posibles_aba(pos):
+
+    i,j = pos
+    cuad = cuadros[i][j]
+    c1 = cuad[C]
+    c2 = cuad[D]
+    posibles = []
+    aux = [(h,k) for h in range(20) for k in range(20)]
+    aux.remove(pos)
+    for q,p in aux:
+        cuad_aux = cuadros[q][p]
+        if np.all(c1 == cuad_aux[A]):
+            if np.all(c2 == cuad_aux[B]):
+                posibles.append((q,p))
+
+    return posibles
+
+def posibles(pos):
+
+    return {"pos": [pos], "izq": posibles_izq(pos), "der": posibles_der(pos),
+            "arr": posibles_arr(pos), "aba": posibles_aba(pos)}
+
+
+def filtro(pos_rel, cant):
+
+    aux = []
+
+    for i in range(20):
+        for j in range(20):
+            pos = posibles((i,j))
+            if len(pos[pos_rel]) == cant:
+                aux.append(pos["pos"][0])
+
+    return aux
 
 
