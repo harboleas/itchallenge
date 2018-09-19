d54d320a54ce3b277f0cbd9963c75d9577e84bbd
Hugo Arboleas <harboleas@citedef.gob.ar>
2018-09-18 22:36:21 -0300

    clean canada

--- 2018/canada.py
+++ 2018/canada.py
@@ -34,93 +34,19 @@ H = (47, 47)
 
 def ordenar(cuadros, orden_mat):
 
-    imag_out = np.zeros((1000,1000,3))
+    imag_out = np.zeros((1000,1000,3),dtype=np.uint8)
 
     for i in range(20):
         for j in range(20):
             ii, jj = orden_mat[i,j]
-            imag_out[i*50:i*50+50,j*50:j*50+50] = cuadros[ii][jj]
+            imag_out[i*50:i*50+50,j*50:j*50+50,:] = cuadros[ii][jj]
 
     return imag_out
 
-orden_mat = np.zeros((20,20,2), dtype = np.int)
+orden_mat = np.zeros((20,20,2), dtype = np.uint8)
 
 
-def gen_posibles_vieja(n, posibles):
-
-    i = n / 20
-    j = n % 20
-
-    aux = [(h,k) for h in range(20) for k in range(20)]
-
-    for t in range(n):
-        h = t / 20
-        k = t % 20
-        aux.remove(tuple(orden_mat[h,k]))
-
-
-    if i == 0 and 0 < j:
-        ii, jj = orden_mat[i,j-1]
-        cuadro = cuadros[ii][jj]
-
-        c1 = cuadro[C]
-        c2 = cuadro[E]
-        c3 = cuadro[H]
-
-        posibles_sig = []
-
-        for p,q in aux:
-            aux_cuad = cuadros[p][q]
-            if np.all(c1 == aux_cuad[A]):
-                if np.all(c2 == aux_cuad[D]):
-                    if np.all(c3 == aux_cuad[F]):
-                        posibles_sig.append((p,q))
-        return posibles_sig
-
-    elif i > 0 and j == 0:
-        ii, jj = orden_mat[i-1,j]
-        cuadro = cuadros[ii][jj]
-
-        c1 = cuadro[F]
-        c2 = cuadro[G]
-        c3 = cuadro[H]
-
-        posibles_sig = []
-
-        for p,q in aux:
-            aux_cuad = cuadros[p][q]
-            if np.all(c1 == aux_cuad[A]):
-                if np.all(c2 == aux_cuad[B]):
-                    if np.all(c3 == aux_cuad[C]):
-                        posibles_sig.append((p,q))
-        return posibles_sig
-
-    else:
-        i1, j1 = orden_mat[i,j-1]
-        i2, j2 = orden_mat[i-1,j]
-
-        cuad1 = cuadros[i1][j1]
-        cuad2 = cuadros[i2][j2]
-
-        c1 = cuad1[C]
-        c2 = cuad1[E]
-        c3 = cuad1[H]
-        c4 = cuad2[G]
-        c5 = cuad2[H]
-
-        posibles_sig = []
-
-        for p,q in aux:
-            aux_cuad = cuadros[p][q]
-            if np.all(c1 == aux_cuad[A]):
-                if np.all(c2 == aux_cuad[D]):
-                    if np.all(c3 == aux_cuad[F]):
-                        if np.all(c4 == aux_cuad[B]):
-                            if np.all(c5 == aux_cuad[C]):
-                                posibles_sig.append((p,q))
-        return posibles_sig
-
-def gen_posibles(n, posibles):
+def gen_posibles(n):
 
     i = n / 20
     j = n % 20
@@ -132,12 +58,11 @@ def gen_posibles(n, posibles):
         k = t % 20
         aux.remove(tuple(orden_mat[h,k]))
 
-
     if i == 0 and 0 < j:
         ii, jj = orden_mat[i,j-1]
         cuadro = cuadros[ii][jj]
 
-        c1 = cuadro[2:47,47]
+        c1 = cuadro[2:47, 47]
 
         posibles_sig = []
 
@@ -162,20 +87,20 @@ def gen_posibles(n, posibles):
         return posibles_sig
 
     else:
-        i1, j1 = orden_mat[i,j-1]
-        i2, j2 = orden_mat[i-1,j]
+        i1, j1 = orden_mat[i, j-1]
+        i2, j2 = orden_mat[i-1, j]
 
         cuad1 = cuadros[i1][j1]
         cuad2 = cuadros[i2][j2]
 
-        c1 = cuad1[2:47,47]
+        c1 = cuad1[2:47, 47]
         c2 = cuad2[47, 2:47]
 
         posibles_sig = []
 
         for p,q in aux:
             aux_cuad = cuadros[p][q]
-            if np.all(c1 == aux_cuad[2:47,2]):
+            if np.all(c1 == aux_cuad[2:47, 2]):
                 if np.all(c2 == aux_cuad[2, 2:47]):
                     posibles_sig.append((p,q))
         return posibles_sig
@@ -183,8 +108,8 @@ def gen_posibles(n, posibles):
 
 def buscar_orden(n=0, posibles=None):
 
-    if n==0:
-        posibles = [(h,k) for h in range(20) for k in range(20)]
+#    if n==0:
+#        posibles = [(h,k) for h in range(20) for k in range(20)]
 
     i = n / 20
     j = n % 20
@@ -192,7 +117,6 @@ def buscar_orden(n=0, posibles=None):
     if n == 20*20-1:
         if posibles:
             orden_mat[i,j] = posibles[0]
-#            print orden_mat
             return orden_mat
         else:
             return None
@@ -201,11 +125,12 @@ def buscar_orden(n=0, posibles=None):
         for x in posibles:
             orden_mat[i,j] = x
             show_imag()
-            posibles_sig = gen_posibles(n+1, posibles)
+            posibles_sig = gen_posibles(n+1)
+            print n, posibles_sig
             aux = buscar_orden(n+1, posibles_sig)
+            print n
             if not (aux is None):
                 return aux
-
 
 def show_imag():
 
